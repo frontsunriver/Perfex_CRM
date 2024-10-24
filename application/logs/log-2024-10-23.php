da465381d1818e6eaa8c92e7569189f52dd14219
Sunriver <seriousman217winter@gmail.com>
2024-10-24 12:12:46 +0900

    feat(:add)initial project

---
+++ application/logs/log-2024-10-23.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-23 22:54:49 --> Severity: Compile Error --> Array and string offset access syntax with curly braces is no longer supported C:\xampp8\htdocs\crm\application\vendor\neitanod\forceutf8\src\ForceUTF8\Encoding.php 196
+ERROR - 2024-10-23 22:56:32 --> Severity: Compile Error --> Array and string offset access syntax with curly braces is no longer supported C:\xampp8\htdocs\crm\application\vendor\neitanod\forceutf8\src\ForceUTF8\Encoding.php 196
